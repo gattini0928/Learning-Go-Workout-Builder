b93a40f98588eee917808782f88d40c829f8df3b
gattini0928 <gabrielgattini659@gmail.com>
2026-03-22 17:01:20 -0300

    Validador de senha e hash de senha criado.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.6
 require (
 	github.com/lib/pq v1.12.0
 	github.com/spf13/viper v1.21.0
+	golang.org/x/crypto v0.49.0
 )
 
 require (
@@ -18,6 +19,6 @@ require (
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 )
--- validators/passwordValidator.go
+++ validators/passwordValidator.go
@@ -1,1 +1,55 @@
 package validators
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"unicode"
+	"fmt"
+)
+
+func ValidatePassword(password string) error {
+
+	if password == "" {
+		return errors.New("senha em branco")
+	}
+
+	if len(password) < 8 {
+		return errors.New("senha deve ter pelo menos 8 caracteres")
+	}
+
+	specials := func(r rune) bool {
+		return unicode.IsPunct(r) || unicode.IsSymbol(r)
+	}
+
+	rules := map[string]func(rune) bool {
+		"letra maiúscula": unicode.IsUpper,
+		"letra minúscula": unicode.IsLower,
+		"dígito": unicode.IsDigit,
+		"caractere especial": specials,
+	}
+
+	for ruleName, ruleFunc := range rules {
+		found := false
+		for _, char := range password {
+			if ruleFunc(char) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return fmt.Errorf("sua senha deve conter pelo menos um(a) %s", ruleName)
+		}
+	}
+
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
