328f7a9563a5a1bec940edf5dc93bcf8344db48d
gattini0928 <gabrielgattini659@gmail.com>
2026-03-23 06:48:45 -0300

    terminando validadores

--- validators/emailValidator.go
+++ validators/emailValidator.go
@@ -1,1 +1,22 @@
 package validators
+
+import (
+	"errors"
+	"net/mail"
+	"strings"
+)
+
+func ValidateEmail(email string) error {
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+        return errors.New("Email inválido")
+    }
+	emailParts := strings.Split(email, "@")
+	domain := emailParts[len(emailParts)-1]
+	charToFind := "."
+
+	if !strings.Contains(domain, charToFind) {
+        return errors.New("Email inválido")
+	}
+	return nil
+}
--- validators/nameValidator.go
+++ validators/nameValidator.go
@@ -1,1 +1,30 @@
 package validators
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+func ValidateName(name string) error {
+	parts := strings.Fields(name)
+	if len(parts) < 2 {
+		return errors.New("nome incompleto")
+	}
+
+	if len(parts[0]) < 2 || len(parts[1]) < 2 {
+		return errors.New("nome ou sobrenome incompletos")
+	}
+
+	if parts[0] == parts[1] {
+		return errors.New("nome e sobrenome iguais")
+	}
+
+	for _, char := range name {
+		if !unicode.IsLetter(char) && char != ' ' {
+			return errors.New("nome só pode conter letras")
+		}
+	}
+
+	return nil
+}
